9a03b06412beff1523ad337a2bbc40855d9f3877
Mobeen <Mobeen.infini8ai@gmail.com>
2026-06-16 17:38:30 +0500

    add dns code

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = require("./src/app");
 
 const connectDB = require("./src/config/db");
 
-// const dns = require("dns");
-// dns.setServers(["8.8.8.8", "1.1.1.1"]);
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 connectDB();
 mongoose.connection.on("connected", () => {
