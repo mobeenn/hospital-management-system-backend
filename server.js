31fa90495ed4ee2e475abaf10483e5f0c2a9b4a9
Mobeen <Mobeen.infini8ai@gmail.com>
2026-06-16 18:23:32 +0500

    remove listen port code

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ mongoose.connection.on("disconnected", () => {
 });
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-   console.log(`Server running on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//    console.log(`Server running on ${PORT}`);
+// });
