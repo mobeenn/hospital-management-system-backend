101e60088123e12c6a4b3c5278b1b6b2555f9762
Mobeen <Mobeen.infini8ai@gmail.com>
2026-06-17 01:59:46 +0500

    add string in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,33 +1,16 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//    try {
-//       await mongoose.connect(process.env.MONGO_URI);
-
-//       console.log("MongoDB Connected");
-//    } catch (error) {
-//       console.log(error.message);
-//       process.exit(1);
-//    }
-// };
-
-// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
    try {
-      console.log(
-         "MONGO_URI exists:",
-         !!process.env.MONGO_URI ||
+      await mongoose.connect(
+         process.env.MONGO_URI ||
             "mongodb+srv://mobeen0616_db_user:CCGdMhFQW5YuBajB@cluster0.akxwptt.mongodb.net/",
       );
 
-      await mongoose.connect(process.env.MONGO_URI);
-
       console.log("MongoDB Connected");
    } catch (error) {
-      console.error("MongoDB Connection Error:");
-      console.error(error);
+      console.log(error.message);
+      process.exit(1);
    }
 };
 
